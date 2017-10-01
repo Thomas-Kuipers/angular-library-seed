30237a6b5cd1049f5bedce08cb2d1304cce9085f
Thomas Kuipers <thomas@webvalid.nl>
2017-10-01 21:01:43 +0200

    unit tests adapative service

--- src/services/adaptive/adaptive.service.spec.ts
+++ src/services/adaptive/adaptive.service.spec.ts
@@ -4,31 +4,35 @@ import {
   getTestBed
 } from '@angular/core/testing';
 
-import {Injector} from '@angular/core';
 import {AdaptiveService} from "./adaptive.service";
 import {DeviceHelper} from "../../helpers/device/device.helper";
 import {OrientationHelper} from "../../helpers/orientation/orientation.helper";
 import {ScreenWidthHelper} from "../../helpers/screen-width/screen-width.helper";
+import {Observable} from "rxjs/Observable";
 
 describe('AdaptiveService', () => {
   let adaptiveService: AdaptiveService;
 
-  const configureTestbed = () => {
+  const configureTestbed = (results: any) => {
     class MockDeviceHelper {
-
+      validate() {
+        return Observable.of(results.devices);
+      }
     }
 
     class MockOrientationHelper {
-
+      validate() {
+        return Observable.of(results.orientation);
+      }
     }
 
     class MockScreenWidthHelper {
+      validateMin() {
+        return Observable.of(results.minScreenWidth);
+      }
 
-    }
-
-    class MockInjector {
-      public get(key) {
-        return 'vfsdvdfs';
+      validateMax() {
+        return Observable.of(results.maxScreenWidth);
       }
     }
 
@@ -38,7 +42,6 @@ describe('AdaptiveService', () => {
         { provide: DeviceHelper, useClass: MockDeviceHelper },
         { provide: OrientationHelper, useClass: MockOrientationHelper },
         { provide: ScreenWidthHelper, useClass: MockScreenWidthHelper },
-        { provide: Injector, useClass: MockInjector },
       ]
     });
 
@@ -46,12 +49,190 @@ describe('AdaptiveService', () => {
     adaptiveService = testBed.get(AdaptiveService);
   };
 
-  it('should emit false when the screen is 200 and the max is 100', async(() => {
-    configureTestbed();
+  it('should emit true when all conditions are true', async(() => {
+    configureTestbed({
+      maxScreenWidth: true,
+      minScreenWidth: true,
+      orientation: true,
+      devices: true
+    });
 
     adaptiveService
       .validate({
-        maxScreenWidth: 100
+        maxScreenWidth: 1,
+        minScreenWidth: 1,
+        orientation: 'portrait',
+        devices: ['tablet']
+      })
+      .subscribe((result) => expect(result).toBe(true));
+  }));
+
+  it('should emit false when at least one of the conditions is false', async(() => {
+    configureTestbed({
+      maxScreenWidth: true,
+      minScreenWidth: true,
+      orientation: true,
+      devices: false
+    });
+
+    adaptiveService
+      .validate({
+        maxScreenWidth: 1,
+        minScreenWidth: 1,
+        orientation: 'portrait',
+        devices: ['tablet']
+      })
+      .subscribe((result) => expect(result).toBe(false));
+  }));
+
+  it('should emit true when an unspecified condition is false', async(() => {
+    configureTestbed({
+      maxScreenWidth: true,
+      minScreenWidth: true,
+      orientation: true,
+      devices: false
+    });
+
+    adaptiveService
+      .validate({
+        maxScreenWidth: 1,
+        minScreenWidth: 1,
+        orientation: 'portrait'
+      })
+      .subscribe((result) => expect(result).toBe(true));
+  }));
+
+  it('should emit false when a custom boolean is false', async(() => {
+    configureTestbed({});
+
+    adaptiveService
+      .validate({
+        custom: [false]
+      })
+      .subscribe((result) => expect(result).toBe(false));
+  }));
+
+  it('should emit true when a custom boolean is true', async(() => {
+    configureTestbed({});
+
+    adaptiveService
+      .validate({
+        custom: [true]
+      })
+      .subscribe((result) => expect(result).toBe(true));
+  }));
+
+  it('should emit false when a custom function returns false', async(() => {
+    configureTestbed({});
+
+    adaptiveService
+      .validate({
+        custom: [() => false]
+      })
+      .subscribe((result) => expect(result).toBe(false));
+  }));
+
+  it('should emit true when a custom function returns true', async(() => {
+    configureTestbed({});
+
+    adaptiveService
+      .validate({
+        custom: [() => true]
+      })
+      .subscribe((result) => expect(result).toBe(true));
+  }));
+
+  it('should emit false when a custom observable emits false', async(() => {
+    configureTestbed({});
+
+    adaptiveService
+      .validate({
+        custom: [Observable.of(false)]
+      })
+      .subscribe((result) => expect(result).toBe(false));
+  }));
+
+  it('should emit true when a custom observable emits true', async(() => {
+    configureTestbed({});
+
+    adaptiveService
+      .validate({
+        custom: [Observable.of(true)]
+      })
+      .subscribe((result) => expect(result).toBe(true));
+  }));
+
+  it('should emit false when a custom promise resolves with false', async(() => {
+    configureTestbed({});
+
+    const promise = new Promise(resolve => {
+      resolve(false);
+    });
+
+    adaptiveService
+      .validate({
+        custom: [promise]
+      })
+      .subscribe((result) => expect(result).toBe(false));
+  }));
+
+  it('should emit true when a custom promise resolves with true', async(() => {
+    configureTestbed({});
+
+    const promise = new Promise(resolve => {
+      resolve(true);
+    });
+
+    adaptiveService
+      .validate({
+        custom: [promise]
+      })
+      .subscribe((result) => expect(result).toBe(true));
+  }));
+
+  it('should emit true when all conditions, including custom ones, return true', async(() => {
+    configureTestbed({
+      maxScreenWidth: true,
+      minScreenWidth: true,
+      orientation: true,
+      devices: true
+    });
+
+    const promise = new Promise(resolve => {
+      resolve(true);
+    });
+
+    adaptiveService
+      .validate({
+        maxScreenWidth: 1,
+        minScreenWidth: 1,
+        orientation: 'portrait',
+        devices: ['tablet'],
+        custom: [promise, Observable.of(true), true, () => true]
+      })
+      .subscribe((result) => expect(result).toBe(true));
+  }));
+
+  it('should emit false when only 1 custom condition is false, but everything else is true', async(() => {
+    configureTestbed({
+      maxScreenWidth: true,
+      minScreenWidth: true,
+      orientation: true,
+      devices: true
+    });
+
+    // the custom promise is the only false value
+    const promise = new Promise(resolve => {
+      resolve(false);
+    });
+
+    adaptiveService
+      .validate({
+        maxScreenWidth: 1,
+        minScreenWidth: 1,
+        orientation: 'portrait',
+        devices: ['tablet'],
+        custom: [promise, Observable.of(true), true, () => true]
       })
       .subscribe((result) => expect(result).toBe(false));
   }));
